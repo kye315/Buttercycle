d87f102a378afe86bc60bb848902f9bffa04b179
kye315 <202764839+kye315@users.noreply.github.com>
2025-09-19 05:26:24 +1000

    Funicle

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -8,13 +8,17 @@ public class Controls : MonoBehaviour
     private Rigidbody wRB;
 
     public GameObject jumper;
+    private Vector3 jScale; // scale at startup
     private ConfigurableJoint jCJ;
+
+    public float jumpSpeed = 4;
 
     private bool jumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        jScale = jumper.transform.localScale;
         wRB = wheel.GetComponent<Rigidbody>();
         jCJ = jumper.GetComponent<ConfigurableJoint>();
     }
@@ -24,7 +28,11 @@ public class Controls : MonoBehaviour
     {
         if (jumping)
         {
-            jumper.transform.localScale += new Vector3(0,1*Time.deltaTime,0);
+            jumper.transform.localScale += new Vector3(0,jumpSpeed*Time.deltaTime,0);
+            if (jumper.transform.localScale.y >= 3) {
+                jumping = false;
+                jumper.transform.localScale = jScale;
+            }
         }
 
         if (Input.GetKey(KeyCode.A))
