37b0ad21bdfbe1106b0542a13f251f71ad071819
Kai Ellem <s5394061@student.ad.griffith.edu.au>
2025-10-03 16:29:26 +1000

    Why didn't these commit

--- Assets/Scripts/Grader.cs
+++ Assets/Scripts/Grader.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Grader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool[] Grades; // left arm, left leg, right arm, right leg
 }
